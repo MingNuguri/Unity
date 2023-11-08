8fa8585e75f60267a66d56347f80f31db772aef4
micat4208@gmail.com <MIcat>
2023-11-08 10:26:23 +0900

    First Commit

---
+++ Assets/SampleScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+	void Awake()
+	{
+	}
+	void Start()
+	{
+	}
+	void OnEnable()
+	{
+	}
+	void OnDisable()
+	{
+	}
+	void FixedUpdate()
+	{
+	}
+	void Update()
+	{
+	}
+	void LateUpdate()
+	{
+	}
+	void OnDestroy()
+	{
+	}
+
+
+}
